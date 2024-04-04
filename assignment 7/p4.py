3f210d804644795551d209aad0ee2b6e3ed2722f
coursera-test <amankum199@gmail.com>
2024-04-05 01:44:05 +0530

    new

--- assignment 7/p4.py
+++ assignment 7/p4.py
@@ -1,1 +1,11 @@
 #Write a program to calculate HCF of two numbers
+x=int(input("Enter first number: "))
+y=int(input("Enter second number: "))
+if x > y:
+        smaller = y
+else:
+        smaller = x
+for i in range(1, smaller+1,1):
+        if((x % i == 0) and (y % i == 0)):
+            hcf = i 
+print(hcf)
