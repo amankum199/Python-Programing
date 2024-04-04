87fbc95c5d908838d817e0c35a055ba966f47de3
coursera-test <amankum199@gmail.com>
2024-04-04 13:04:21 +0530

    new

---
+++ assignment 4/p5.py
@@ -0,0 +1,3 @@
+#Write a program to print the first 10 odd natural numbers in reverse order.
+for i in range(19, 0, -2):
+    print(i)
---
+++ assignment 4/p6.py
@@ -0,0 +1,3 @@
+#Write a program to print the first 10 even natural numbers
+for i in range(2,21,2):
+ print(i)
