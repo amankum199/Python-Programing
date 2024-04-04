e9dda600641a1f30e39b52a5798ebab80a4a04c4
coursera-test <amankum199@gmail.com>
2024-04-05 03:13:45 +0530

    new

--- assignment 7/p5.py
+++ assignment 7/p5.py
@@ -1,1 +1,15 @@
 #Write a program to check whether two given numbers are co-prime numbers or not
+num1=int(input("Enter number 1: "))
+num2=int(input("Enter number 2: "))
+if num1>num2 :
+    greater=num1
+else:
+    greater=num2    
+for i in range(2,greater+1):
+    if((num1%i==0) and (num2%i==0)):
+     print("Not Co-prime")
+    else:
+     print("Co-prime")
+    break
+
+
